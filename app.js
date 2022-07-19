af99e8a15d1a7b4230d0f834f9c6b623df440f51
William Luna <wluna98@gmail.com>
2022-07-19 11:48:05 -0700

    Added Error Handling/Data Validation + Reviews

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const { shopSchema } = require('./schemas.js');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Shop = require('./models/shop');
 
@@ -25,42 +28,65 @@ app.engine('ejs', ejsMate);
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
-app.get('/shops', async (req, res) => {
+const validateShop = (req, res, next) => {
+    const { error } = shopSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+app.get('/shops', catchAsync(async (req, res) => {
     const shops = await Shop.find({});
     res.render('shops/index', { shops });
-});
+}));
 
-app.get('/shops/new', async (req, res) => {
+app.get('/shops/new', catchAsync(async (req, res) => {
     res.render('shops/new');
-})
+}));
 
-app.post('/shops', async (req, res) => {
+app.post('/shops', validateShop, catchAsync(async (req, res, next) => {
+
     const shop = new Shop(req.body.shop);
     await shop.save();
     res.redirect(`/shops/${shop._id}`);
-});
+}));
 
-app.get('/shops/:id', async (req, res) => {
+app.get('/shops/:id', catchAsync(async (req, res) => {
     const shop = await Shop.findById(req.params.id);
     res.render('shops/show', { shop });
-});
+}));
 
-app.get('/shops/:id/edit', async (req, res) => {
+app.get('/shops/:id/edit', catchAsync(async (req, res) => {
     const shop = await Shop.findById(req.params.id);
     res.render('shops/edit', { shop });
-});
+}));
 
-app.put('/shops/:id', async (req, res) => {
+app.put('/shops/:id', validateShop, catchAsync(async (req, res) => {
     const { id } = req.params;
     const shop = await Shop.findByIdAndUpdate(id, { ...req.body.shop });
     res.redirect(`/shops/${shop._id}`);
-});
+}));
 
-app.delete('/shops/:id', async (req, res) => {
+app.delete('/shops/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     const shop = await Shop.findByIdAndDelete(id);
     res.redirect('/shops');
-})
+}));
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+});
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something Went Wrong!';
+    res.status(statusCode).render('error', { err });
+});
+
+
 
 app.listen(3000, () => {
     console.log('Listening on Port 3000...');
